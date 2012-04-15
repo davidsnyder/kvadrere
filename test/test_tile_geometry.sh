7716b9306d24eb3aecebf4e32651ec0279f69099
David Snyder <dsnyder90@gmail.com>
2012-04-15 17:29:44 -0500

    updating README

--- test/test_tile_geometry.sh
+++ test/test_tile_geometry.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pig -x local -p IN=data/admin0_MX.geojson -p OUT=mx_test/ tile_geometries.pig
-cut -f2 mx_test/* | ./features_to_collection.sh > mexico_tiles_Z11.geojson
+pig -x local -p IN=data/admin0_MX.geojson -p OUT=admin0_MX_tiled_Z11/ tile_geometries.pig
+cut -f2 admin0_MX_tiled_Z11/* | ./features_to_collection.sh > mexico_tiles_Z11.geojson
